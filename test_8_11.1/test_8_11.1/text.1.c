a87fe5cd546fc9685edd758b8e808f828e936dfd
jie-chen532 <3479676393@qq.com>
2020-08-12 23:27:11 +0800

    操作符、关键字

---
+++ test_8_11.1/test_8_11.1/text.1.c
@@ -0,0 +1,11 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+#define _CRT_SECURE_NO_WARNINGS 1
+
+static int  g_gl = 520;
+
+static int Add(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
---
+++ test_8_11/test_8_11/text1.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+int  g_gl = 520;
+
+ int Add(int x, int y)
+{
+	int z = x + y;
+	return z;
+}
